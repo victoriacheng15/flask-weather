6e62c3cab9d9555843488e6fa2435329dca3657b
Victoria Cheng <victoria.cheng15@gmail.com>
2024-03-18 10:07:35 -0600

    chore: render index and weather pages

--- main.py
+++ main.py
@@ -6,7 +6,26 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello world"
+    return render_template("index.html")
+
+
+@app.route("/weather")
+def get_weather():
+    city = request.args.get("city")
+    country = request.args.get("country")
+    weather_data = get_current_weather(city, country)
+
+    return render_template(
+        "weather.html",
+        title=weather_data["name"],
+        country=weather_data["sys"]["country"],
+        status=weather_data["weather"][0]["description"].capitalize(),
+        temp=f"{weather_data['main']['temp']:.1f}",
+        min_temp=f"{weather_data['main']['temp_min']:.1f}",
+        max_temp=f"{weather_data['main']['temp_max']:.1f}",
+        feels_like=f"{weather_data['main']['feels_like']:.1f}",
+        humidity=f"{weather_data['main']['humidity']}",
+    )
 
 
 if __name__ == "__main__":
