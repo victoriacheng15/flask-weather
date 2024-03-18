43ff018a51395aea07a291f104f3360588758058
Victoria Cheng <victoria.cheng15@gmail.com>
2024-03-17 19:11:07 -0600

    chore: set up index.html and main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+from weather import get_current_weather
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+  return "Hello world"
+
+
+
+if __name__ == "__main__":
+  app.run(port=8000)
