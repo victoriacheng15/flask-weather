61b925526aac28a6b015c823a5066fc729b5884d
Victoria Cheng <victoria.cheng15@gmail.com>
2024-03-18 10:04:52 -0600

    chore: add country parameter

--- weather.py
+++ weather.py
@@ -7,7 +7,7 @@ load_dotenv()
 API_KEY = os.getenv("WEATHER_API_KEY")
 
 
-def get_current_weather(city):
-    URL = f"http://api.openweathermap.org/data/2.5/weather?appid={API_KEY}&q={city}&units=metric"
+def get_current_weather(city, country):
+    URL = f"http://api.openweathermap.org/data/2.5/weather?appid={API_KEY}&q={city},{country}&units=metric"
     weather_data = requests.get(URL).json()
     return weather_data
