f2abbd14d33b844e9bb1073267c4bc1ae5f903f9
Victoria Cheng <victoria.cheng15@gmail.com>
2024-03-26 19:48:32 -0600

    feat: add weather test with pytest

---
+++ tests/test_weather.py
@@ -0,0 +1,25 @@
+import pytest
+import requests
+import unittest.mock as mock
+import weather
+
+@mock.patch("requests.get")
+def test_weather(mock_get):
+  mock_response = mock.Mock()
+  mock_response.status_code = 200
+  mock_response.json.return_value = {
+    "name": "Test City",
+    "sys": {"country": "TC"},
+    "weather": [{"description": "cloudy"}],
+    "main": {
+      "temp": 20.5,
+      "temp_min": 18.5,
+      "temp_max": 22.5,
+      "feels_like": 21.0,
+      "humidity": 70
+    }
+  }
+
+  mock_get.return_value = mock_response
+  weather_data = weather.get_current_weather("Test City", "TC")
+  assert weather_data == mock_response.json()
